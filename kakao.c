d7cf12c92d1e0e768c321a0f43dc8a8b87cce11c
SH <22000786@handong.edu>
2021-05-02 00:05:35 +0900

    modify save_data func

--- kakao.c
+++ kakao.c
@@ -108,7 +108,7 @@ void save_data(Kakao * k[], int count){
 	fp = fopen("kakao.txt","wt");
 	for(int i=0; i<count; i++){
 		if(k[i]->birth_month==-1 || k[i]->birth_day==-1) continue;
-		fprintf(fp,"%d %d %d %d %s\n%s\n%s\n", k[i]->birth_month, k[i]->birth_day, k[i]->is_kakaostory, k[i]->is_bookmark, k[i]->name, k[i]->phone_num, k[i]->profile_music);
+		fprintf(fp,"%d %d %d %d%s\n%s\n%s\n", k[i]->birth_month, k[i]->birth_day, k[i]->is_kakaostory, k[i]->is_bookmark, k[i]->name, k[i]->phone_num, k[i]->profile_music);
 	}
 	fclose(fp);
 	printf("=> 저장됨 !!\n");
