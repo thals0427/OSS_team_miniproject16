675637ab2603c33ffb785042b3d898a7add79791
somin <zzozzo0427@gmail.com>
2021-05-09 21:17:47 +0900

    add update funtion

--- kakao.c
+++ kakao.c
@@ -48,4 +48,41 @@ void list_k(Kakao * k[], int index){
 		printf("%2d ", i+1);
 		read(*k[i]);
 	}
-}	
+}
+
+int update_num(Kakao * k[], int index){
+	list_k(k, index);
+	printf("수정할 친구 번호는? ");
+	int num;
+	scanf("%d", &num);
+	return num;
+}
+
+int update(Kakao * k){
+
+	printf("수정할 친구의 이름은? ");
+	fgets(k->name,100,stdin);
+	k->name[strlen(k->name)-1]= '\0';
+
+	printf("수정할 친구의 생일은? ex) 05 25 ");
+	scanf("%d %d", &k->birth_month, &k->birth_day);
+
+	printf("수정할 친구의 카스 유무는? (No: 0) ");
+	int n1;
+	scanf("%d", &n1);
+	k->is_kakaostory = n1;
+
+	getchar();
+	printf("수정할 친구의 전화번호는? ");
+	scanf("%[^\n]s", k->phone_num);
+
+	getchar();
+	printf("수정할 친구의 프로필뮤직은? ");
+	scanf("%[^\n]s", k->profile_music);
+
+	printf("수정할 친구의 즐겨찾기 유무는? (No: 0) ");
+	int n2;
+	scanf("%d", &n2);
+	k->is_bookmark = n2;
+	return 1;
+}
