75c6f605e87293e452d3f12718f355a3dec5c80a
somin <zzozzo0427@gmail.com>
2021-05-17 00:51:46 +0900

    update main

--- search.h
+++ search.h
@@ -1,5 +1,5 @@
 #include "kakao.h"
 void search_name(Kakao * k[], int index);
-void serach_bd(Kakao * k[], int index);
-void serach_phone_num(Kakao * k[], int index);
+void search_bd(Kakao * k[], int index);
+void search_phone_num(Kakao * k[], int index);
 
